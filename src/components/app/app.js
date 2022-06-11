825acad668aa6b5019c42ebd2c27e8658d28c3fb
Alex2 <aleksander1802@gmail.com>
2022-06-11 15:25:41 +0300

    init: test

---
+++ src/components/app/app.js
@@ -0,0 +1,41 @@
+
+import AppInfo from '../app-info/app-info'
+import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployeesList from '../employees-list/employees-list';
+import EmployeesAddForm from '../employees-add-form/employees-add-form';
+
+
+
+
+
+import './app.css'
+
+function App() {
+
+  const data = [
+    {name: 'John', salary: 800, increase: true, id: '1', rise: false},
+    {name: 'Smith', salary: 1400, increase: false, id: '2', rise: false},
+    {name: 'Smoth', salary: 5600, increase: false, id: '3', rise: false}
+];
+
+  return(
+    <div className="app">
+      <AppInfo/> 
+      <div className="search-panel">
+        <SearchPanel/>
+        <AppFilter/>         
+      </div>      
+      <EmployeesList data={data}/>   
+      <EmployeesAddForm/>      
+    </div>
+  )
+}
+
+export default App;
+
+
+
+
+
+
